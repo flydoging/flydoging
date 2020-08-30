ed7ec7dd5abadd06edea89d5e747ac647c081627
flydoging <jinke124578@outlook.com>
2020-08-31 00:49:30 +0800

    mongo document pojo

---
+++ src/main/java/com/flydoging/spring_data/pojo/Document.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2020 Baidu, Inc. All Rights Reserved.
+ */
+package com.flydoging.spring_data.pojo;
+
+import org.springframework.data.annotation.Id;
+
+import lombok.Data;
+
+@Data
+public class Document {
+    @Id
+    private Long id;
+    private String content;
+    private Long createBy;
+    private Long createTime;
+    private Long updateBy;
+    private Long updateTime;
+}
