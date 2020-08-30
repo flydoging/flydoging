23d327e470160c7f885050d4ec03cc6f42d90684
flydoging <jinke124578@outlook.com>
2020-08-31 00:15:52 +0800

    first commit

---
+++ src/test/java/com/flydoging/spring_data/SpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flydoging.spring_data;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
